e526e6fc37492ea93e4b7ac4434e1bc8d20fdb3a
godsevenzeroseven <36392045+godsevenzeroseven@users.noreply.github.com>
2018-02-11 20:57:57 -0600

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+
+const TOKEN = "NDEyNDAzNjQ4NDM0NDA1Mzc2.DWJ0SQ.x7EBXKN3Y4bOAjTUsfewlHNtS_8";
+
+var schedule = require('node-schedule');
+var bot = new Discord.Client();
+
+bot.on('ready', () => {
+    var channel = bot.channels.get("name", "general");
+
+    var rule = new schedule.RecurrenceRule();
+    rule.minute = 7;
+ 
+    var j = schedule.scheduleJob(rule, function(){
+    bot.channels.get("id", channel).sendMessage("Master, master! Please EAT SOMETHING right meow!");
+    })
+
+    console.log("Bot is ready.");
+});
+
+bot.login(process.env.BOT_TOKEN);
